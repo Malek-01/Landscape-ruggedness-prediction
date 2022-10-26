07640eebe3386a88737de6b160454f2eb014f87f
Malek-01 <36338230+Malek-01@users.noreply.github.com>
2022-10-26 17:52:58 +0200

    Add files via upload

---
+++ Random TSP instances/200 cities2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 13:15:46 2020
+
+@author: Malek
+"""
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+import mlrose
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf
+from scipy.stats import entropy
+import pandas as pd
+import random
+import tsplib95
+
+fitness = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+entrop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+for k in range(30):
+    dist = np.zeros((200,200))
+    for i in range(199):
+        for j in range(i+1, 200):
+           dist[i][j] = random.uniform(1,5)  
+    dist_list = [(0,0,0) for x in range(19900)]  
+    l = 0
+    while l < 19900:   
+        for i in range(199):
+            for j in range(i+1, 200):
+                dist_list[l] = (i,j, dist[i][j])
+                print(dist_list[l])
+                l = l+1
+    print(dist_list)
+    # Initialize fitness function object using dist_list
+    fitness_dists = mlrose.TravellingSales(distances = dist_list)
+    
+    # Define optimization problem object
+    problem_fit = mlrose.TSPOpt(length = 200, fitness_fn = fitness_dists, maximize=False)    
+    best_state, best_fitness, fitness[k] = mlrose.random_walk(problem_fit)            
+    
+    print('The best state found is: ', best_state)
+    print('The fitness at the best state is: ', best_fitness)
+    print("fitness")
+    print(fitness[k])
+    entrop[k] = entropy(fitness[k], base=6)
+    print("Solutions")
+    print(entrop[k])
+
+print(entrop)
+df = pd.DataFrame(fitness)
+df.to_excel("TSP_Random_200.xlsx", index=True, startrow=0)
+
---
+++ Random TSP instances/50 cities2.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 21 13:15:46 2020
+
+@author: Malek
+"""
+import six
+import sys
+sys.modules['sklearn.externals.six'] = six
+import mlrose
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf
+from scipy.stats import entropy
+import pandas as pd
+import random
+import tsplib95
+
+fitness = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+entrop = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+for k in range(30):
+    dist = np.zeros((50,50))
+    for i in range(49):
+        for j in range(i+1, 50):
+           dist[i][j] = random.uniform(1,5)  
+    dist_list = [(0,0,0) for x in range(1225)]  
+    l = 0
+    #for l in range(1000):
+    while l < 1225:   
+        for i in range(49):
+            for j in range(i+1, 50):
+                dist_list[l] = (i,j, dist[i][j])
+                print(dist_list[l])
+                l = l+1
+    print(dist_list)
+    # Initialize fitness function object using dist_list
+    fitness_dists = mlrose.TravellingSales(distances = dist_list)
+    
+    # Define optimization problem object
+    problem_fit = mlrose.TSPOpt(length = 10, fitness_fn = fitness_dists, maximize=False)
+
+    best_state, best_fitness, fitness[k] = mlrose.random_walk(problem_fit)            
+    print(best_state)
+    
+    print(best_fitness)
+    
+    print('The best state found is: ', best_state)
+    print('The fitness at the best state is: ', best_fitness)
+    print("The fitness curve is:", fitness[k])
+    entrop[k] = entropy(fitness[k], base=6)    
+    print("The entropy for th k excutions is:", entrop[k])
+
+
+print(entrop)
+df = pd.DataFrame(entrop)
+df.to_excel("TSP_random_50.xlsx", index=True, startrow=0)
+
+#pA = test / test.sum()
+#Shannon2 = -np.sum(pA*np.log6(pA))
+
+#test = pd.Series(sm.tsa.acf(test, nlags=100))
+#df = pd.DataFrame(test)
+#test2 = pd.Series(sm.tsa.acf(test2, nlags=100))
+#df2 = pd.DataFrame(test2)
+#plot_acf(test, lags=100)
+
+#df.to_excel("TSP_LH31.xlsx", index=True, startrow=0)
